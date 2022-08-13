3667f071f1976faa32f8c6b2a013134f3c74be0f
Daniel Beltran <genserdanie-123@hotmail.com>
2022-08-13 16:04:05 +0200

    Compartir información con Spring MVC y Thymeleaf

--- HolaMundoThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
+++ HolaMundoThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
@@ -1,17 +1,24 @@
 package mx.com.gm;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @Slf4j
 public class ControladorInicio {
+    
+    @Value("${index.saludo}")
+    private String saludo;
 
     @GetMapping("/")
-    public String inicio() {
+    public String inicio(Model model) {
+        var mensaje = "HolaMundo con thymeleaf";
         log.info("Ejecutando el controlador de tipo Controlador MVC");
+        model.addAttribute("mensaje", mensaje);
+        model.addAttribute("saludo", saludo);
         return "index";
 
     }
